0194f789846ba6dc2e2e6742673b7c9e5a5467d9
MACRODAT <macrodatservices@gmail.com>
2023-08-20 10:35:36 +0100

    Changes to the file

--- writeFunction.c
+++ writeFunction.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 		{"%", _print_percent}, {"c", _print_char}, {"s", _print_str}, {NULL, NULL}
 	};
 
-	if (!format)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
 	successWrites = continueFunction(format, args, protos);
