39328cb21c3447822983e0f486cd052498d3e668
MACRODAT <macrodatservices@gmail.com>
2023-08-20 04:13:03 +0100

    Changes to the file

---
+++ test/main.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+#include "../writeFunction.c"
+
+int main(void)
+{
+	int success;
+	char *s = "hello world\n%s\n2-%s\nfinal %%words.\0";
+	char *a1 = "Argument 1 processed.\0";
+	char *a2 = "Argument 2 processed.\0";
+	success = _printf(s, a1, a2);
+	// _printf('%d', success);
+	return (success);
+}
